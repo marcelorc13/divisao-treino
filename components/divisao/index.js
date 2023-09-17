2c4152e17ad03e1c47b900644e48d616eff49293
Marcelo <marcelorcarnauba@gmail.com>
2023-09-17 04:59:44 -0300

    criando-funcionalidade-para-mostrar-treino-do-dia

--- components/divisao/index.js
+++ components/divisao/index.js
@@ -1,6 +1,9 @@
 'use client'
 
+import './divisao.css'
+
 import Link from 'next/link'
+import { useEffect } from 'react'
 
 import { CgGym } from 'react-icons/cg'
 import { IoIosBasketball } from 'react-icons/io'
@@ -9,86 +12,113 @@ import { MdDirectionsBike } from 'react-icons/md'
 import { FaSwimmer } from 'react-icons/fa'
 import { RiBoxingFill } from 'react-icons/ri'
 import { userAgent } from 'next/server'
-import Segunda from '../treinos/segunda'
-
 
 export default function Divisao() {
 
+    useEffect(() => {
+        verDia()
+    })
+
+    function verDia() {
+        const dataHoje = new Date()
+        const diaHoje = dataHoje.getDay()
+
+        const dias = document.querySelectorAll('.dias')
+
+        const htmlDia = dias[diaHoje].children[0]
+
+
+        htmlDia.innerHTML = `&#9733; ${htmlDia.innerHTML} &#9733;`
+    }
+
+
     return (
-        <main className="flex flex-col items-center">
-            <div className='flex flex-row items-center justify-center gap-8 py-10 text-4xl font-semibold'>
+        <main className="flex flex-col items-center pb-10">
+            <div className='flex flex-col items-center justify-center gap-1 py-10 text-xl md:text-5xl font-semibold'>
                 <h1>Divisão de Treino</h1>
                 <h2>Equipe Roca</h2>
             </div>
 
-
-            {/* Usar rotas Dinâmicas */}
-            <ul className='flex flex-col list-disc text-2xl'>
-                <li>
-                    <Link href={{
-                        pathname: '/treino',
-                        query: {
-                            dia: 'Segunda-Feira',
-                            musculos: 'Perna Completa',
-                        }
-                    }}>Segunda-Feira : Perna Completa</Link>
-                </li>
-
-                <li>
-                    <Link href={{
-                        pathname: '/treino',
-                        query: {
-                            dia: 'Terça-Feira',
-                            musculos: 'Peito & Bíceps / Panturrilha',
-                        }
-                    }}>Terça-Feira : Peito & Bíceps / Panturrilha</Link>
-                </li>
-                <li>
-                    <Link href={{
-                        pathname: '/treino',
-                        query: {
-                            dia: 'Quarta-Feira',
-                            musculos: 'Costas & Ombro',
-                        }
-                    }}>Quarta-Feira : Costas & Ombro</Link>
-                </li>
-                <li>
-                    <Link href={{
-                        pathname: '/treino',
-                        query: {
-                            dia: 'Quinta-Feira',
-                            musculos: 'Descanso / Panturrilha',
-                        }
-                    }}>Quinta-Feira : Descanso / Panturrilha</Link>
-                </li>
-                <li>
-                    <Link href={{
-                        pathname: '/treino',
-                        query: {
-                            dia: 'Sexta-Feira',
-                            musculos: 'Bíceps & Tríceps',
-                        }
-                    }}>Sexta-Feira : Bíceps & Tríceps</Link>
-                </li>
-                <li>
-                    <Link href={{
-                        pathname: '/treino',
-                        query: {
-                            dia: 'Sábado',
-                            musculos: 'Peito & Ombro / Panturrilha',
-                        }
-                    }}>Sábado : Peito & Ombro / Panturrilha</Link>
-                </li>
-                <li>
+            <ul id='dias' className='flex flex-col gap-6'>
+                <li className='dias'>
                     <Link href={{
                         pathname: '/treino',
                         query: {
                             dia: 'Domingo',
                             musculos: 'Descanso',
                         }
-                    }}>Domingo : Descanso</Link>
+                    }}>
+                        Domingo : Descanso
+                    </Link>
                 </li>
 
+                <li className='dias'>
+                    <Link href={{
+                        pathname: '/treino',
+                        query: {
+                            dia: 'Segunda-Feira',
+                            musculos: 'Perna Completa',
+                        }
+                    }}>
+                        Segunda-Feira : Perna Completa
+                    </Link>
+                </li>
+
+                <li className='dias'>
+                    <Link href={{
+                        pathname: '/treino',
+                        query: {
+                            dia: 'Terça-Feira',
+                            musculos: 'Peito & Bíceps / Panturrilha',
+                        }
+                    }}>
+                        Terça-Feira : Peito & Bíceps / Panturrilha
+                    </Link>
+                </li>
+                <li className='dias'>
+                    <Link href={{
+                        pathname: '/treino',
+                        query: {
+                            dia: 'Quarta-Feira',
+                            musculos: 'Costas & Ombro',
+                        }
+                    }}>
+                        Quarta-Feira : Costas & Ombro
+                    </Link>
+                </li>
+                <li className='dias'>
+                    <Link href={{
+                        pathname: '/treino',
+                        query: {
+                            dia: 'Quinta-Feira',
+                            musculos: 'Descanso / Panturrilha',
+                        }
+                    }}>
+                        Quinta-Feira : Descanso / Panturrilha
+                    </Link>
+                </li>
+                <li className='dias'>
+                    <Link href={{
+                        pathname: '/treino',
+                        query: {
+                            dia: 'Sexta-Feira',
+                            musculos: 'Bíceps & Tríceps',
+                        }
+                    }}>
+                        Sexta-Feira : Bíceps & Tríceps
+                    </Link>
+                </li>
+                <li className='dias'>
+                    <Link href={{
+                        pathname: '/treino',
+                        query: {
+                            dia: 'Sábado',
+                            musculos: 'Peito & Ombro / Panturrilha',
+                        }
+                    }}>
+                        Sábado : Peito & Ombro / Panturrilha
+                    </Link>
+                </li>
             </ul>
         </main >
     )
