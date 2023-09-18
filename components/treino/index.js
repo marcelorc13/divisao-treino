9e2b529000b17069f6173067ea316c9272e910a0
Marcelo <marcelorcarnauba@gmail.com>
2023-09-18 12:40:08 -0300

    criando-hook-e-pagina-dos-treinos

--- components/treino/index.js
+++ components/treino/index.js
@@ -1,21 +1,36 @@
 'use client'
+import TreinoPernaCompleto from '../treinos/treinoSegunda'
 
 import { useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
+import { useState } from 'react'
 
 export default function TreinoBody() {
+
+    useEffect(() => {
+        MostrarParams();
+    }, [''])
+
+
+    function MostrarParams() {
+        if (musculos == 'Perna Completa') {
+            const teste = <TreinoPernaCompleto />
+            setExercicios(teste)
+        }
+    }
 
     const searchParams = useSearchParams()
 
     const dia = searchParams.get('dia')
     const musculos = searchParams.get('musculos')
-    const exercicios = searchParams.get('exercicios')
+
+    const [exercicios, setExercicios] = useState('')
 
 
     return (
         <main className='flex flex-col items-center'>
-            <h1 className='text-2xl md:text-4xl'>{dia} - {musculos}</h1>
-            <div>{exercicios}</div>
+            <h1 className='text-2xl md:text-4xl text-center font-bold'>{dia} - {musculos}</h1>
+            <section className='w-full'>{exercicios}</section>
         </main>
     )
 }
---
+++ components/treinos/treinoSegunda/index.js
@@ -0,0 +1,35 @@
+import '../treinos.css'
+
+export default function TreinoPernaCompleto() {
+    return (
+        <div id="lista-exercicios">
+            <ul>
+                <li>
+                    Cadeira Flexora
+                    <p>3 Séries</p>
+                </li>
+
+                <li>
+                    Leg 45°
+                    <p>3 Séries</p>
+                </li>
+
+                <li>
+                    Agachamento Hack
+                    <p>2 Séries</p>
+                </li>
+
+                <li>
+                    Cadeira Extensora
+                    <p>2 Séries</p>
+                </li>
+
+                <li>
+                    Mesa Flexora
+                    <p>1 Série</p>
+                </li>
+
+            </ul>
+        </div>
+    )
+}
