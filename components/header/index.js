84fafc377aec167d37fe4d3b5c48b4d23d4fb087
Marcelo <marcelorcarnauba@gmail.com>
2023-10-10 08:50:00 -0300

    finalizando-responsividade-do-header

--- components/header/index.js
+++ components/header/index.js
@@ -15,40 +15,45 @@ export default function Header() {
     const abrirMenu = () => {
         const items = document.querySelector('#navItems')
         const lista = document.querySelector('#navLista')
-        const btn = document.querySelector('#btnLogout')
+        const btn = document.querySelector('#btnAbrir')
 
-        console.log(items, lista, btn.innerHTML)
+        if (btn.value == 'abrir') {
+            items.classList.remove('hidden')
+            items.classList.add('pt-6')
 
-        // items.classList.toggle('hidden')
-        // items.classList.toggle('justify-between')
-        // items.classList.toggle('justify-center')
-        // items.classList.toggle('text-center')
-        // items.classList.toggle('flex-col')
+            btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>`
 
-        // lista.classList.toggle('gap-0')
-        // lista.classList.toggle('flex-row')
-        // lista.classList.toggle('flex-col')
-        // console.log(items)
+            btn.value = 'fechar'
+        }
+        else if (btn.value == 'fechar') {
+            items.classList.add('hidden')
+            items.classList.remove('pt-6')
+
+            btn.innerHTML = `<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"></path></svg>`
+
+            btn.value = 'abrir'
+        }
     }
 
     return (
         <nav className="pt-8 md:px-20 md:py-8">
             <div className='flex text-center justify-center md:hidden'>
-                <button className='text-2xl' onClick={abrirMenu}><GiHamburgerMenu/></button>
+                <button id='btnAbrir' className='text-2xl' value='abrir' onClick={abrirMenu}><GiHamburgerMenu /></button>
             </div>
-            <div id='navItems' className='hidden md:flex md:flex-row items-center justify-between md:text-lg lg:text-xl'>
-                <div>
-                    <h1 className='md:text-2xl lg:text-4xl font-semibold'>Equipe Roca</h1>
+            <div id='navItems' className='hidden md:flex md:flex-row items-center justify-between md:text-lg lg:text-xl font-semibold'>
+                <div className='text-center'>
+                    <h1 className='md:text-2xl lg:text-4xl font-bold'>Equipe Roca</h1>
                 </div>
 
                 <div>
-                    <ul id='navLista' className="flex flex-row justify-center gap-4">
+                    <ul id='navLista' className="flex flex-col md:flex-row justify-center items-center gap-0 md:gap-4">
                         <li>Treino de Hoje</li>
                         <li>Guardar Treino</li>
                     </ul>
                 </div>
 
-                <div className='flex gap-2 items-center justify-center'>
+                <div className='flex flex-row items-center justify-center'>
+                    <label htmlFor='btnLogout' className='flex flex-row items-center justify-center cursor-pointer md:hidden'>Logout:</label>
                     <button id='btnLogout' onClick={Logout}><BiLogOut /></button>
                 </div>
             </div>
